7a8e8012269eb228ae9096fdc57251b289105573
nortiza1 <nortiza1@eafit.edu.co>
2024-10-28 16:57:44 -0500

    Reseña.java

--- Reseña.java
+++ Reseña.java
@@ -2,33 +2,28 @@ import java.util.ArrayList;
 
 public class Reseña {
 
-    public static class Datos {
+    private String nombre;
+    private int calificacion;
 
-        private String nombre;
-        private int calificacion;
+    public Reseña(String nombre, int calificacion) {
+        this.nombre = nombre;
+        this.calificacion = calificacion;
+    }
 
-        public Datos(String nombre, int calificacion) {
-            this.nombre = nombre;
-            this.calificacion = calificacion;
-        }
+    public String getNombre() {
+        return nombre;
+    }
 
-        public String getNombre() {
-            return nombre;
-        }
+    public int getCalificacion() {
+        return calificacion;
+    }
 
-        public int getCalificacion() {
-            return calificacion;
-        }
-
-        public String toString() {
-            // Usar el Idioma.getTexto() para mostrar las traducciones adecuadas
-            return Idioma.getTexto(8) + ": " + nombre + ", " + Idioma.getTexto(9) + ": " + calificacion;
-        }
-
+    public String toString() {
+        return Idioma.getTexto(8) + ": " + nombre + ", " + Idioma.getTexto(9) + ": " + calificacion;
     }
 
     //Almacenar Reseña
-    private ArrayList<Datos> reseñas;
+    private ArrayList<Reseña> reseñas;
 
     public Reseña() {
         reseñas = new ArrayList<>();
@@ -36,12 +31,12 @@ public class Reseña {
 
     //Agregar Reseña
     public void agregarReseña(String nombre, int calificacion) {
-        Datos nuevaReseña = new Datos(nombre, calificacion);
+        Reseña nuevaReseña = new Reseña(nombre, calificacion);
         reseñas.add(nuevaReseña);
     }
 
     //Método Ver Reseña
-    public ArrayList<Datos> obtenerReseñas() {
+    public ArrayList<Reseña> obtenerReseñas() {
         return reseñas;
     }
 }
